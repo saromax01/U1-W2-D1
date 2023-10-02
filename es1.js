4ba74005dc6d018af66d686e004a847a04e3ffbf
rosario scilipoti <rosariosca12@icloud.com>
2023-10-02 18:10:37 +0200

    the-end

--- es1.js
+++ es1.js
@@ -13,36 +13,36 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 //esercizio1://
-// I PRINCIPALI DATATYPE SONO: script , number , boolean , undefined e null
-// script non è altro che le parole e deve essere applicato con le seguenti virgolette:  ˜ ˜ "" '' .
-//numer sono i numeri e devono essere applicati senza virgolette cosi per come vengono scritti.
-//boolean sono coloro che ci dicono se la risposta e vero o falsa e si indentificano con la parola (true o false).
-//undefined e quando lasciamo vuoto un contentitore.
-//null e quando lasciamo un contentinore vuoto di proposito per modificarlo in seguito. e ci permette di farlo apprendere anche a chi lo legge.  
+// I PRINCIPALI DATATYPE SONO: string , number , boolean , undefined e null
+// string non è altro che le parole e deve essere applicato con le seguenti virgolette:  ˜ ˜ "" '' .
+// number sono i numeri e devono essere applicati senza virgolette cosi per come vengono scritti.
+// boolean sono coloro che ci dicono se la risposta e vero o falsa e si indentificano con la parola (true o false).
+// undefined e quando lasciamo vuoto un contentitore.
+// null e quando lasciamo un contentinore vuoto di proposito per modificarlo in seguito. e ci permette di farlo apprendere anche a chi lo legge.
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
- //esercizio 2:
-let name = "rosario"
-console.log (name)
+//esercizio 2:
+let name = "rosario";
+console.log(name);
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let number = 12+20
-console.log (number)
+let number = 12 + 20;
+console.log(number);
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let x = 12
-console.log (x)
+let x = 12;
+console.log(x);
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
@@ -50,20 +50,20 @@ console.log (x)
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-name = "scilipoti"
-console.log(name)
+name = "scilipoti";
+console.log(name);
 
-const nameLost = "edoardo"
-// nameLost = "giovanni" esplosione 
-console.log (nameLost)
+const nameLost = "edoardo";
+// nameLost = "giovanni" esplosione
+console.log(nameLost);
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-x = x- 4
-console.log (x)
+let y = x - 4;
+console.log(y);
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -74,14 +74,14 @@ console.log (x)
 /* SCRIVI QUI LA TUA RISPOSTA */
 // creazione stringhe
 
-let name1 = "john"
-console.log (name1)
+let name1 = "john";
+console.log(name1);
 
-let name2 = "John"
-console.log (name2)
- 
-//verifica uguaglianza
-console.log (name1===name2)
+let name2 = "John";
+console.log(name2);
+
+//verifica che sono diversi.
+console.log(name1 !== name2);
 
 //extra
-console.log (name1 .toLowerCase()===name2.toLowerCase())
+console.log(name1.toLowerCase() === name2.toLowerCase());
